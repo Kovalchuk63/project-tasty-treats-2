e19db507f0df9fdbe53738f3586d8ab1d7ba90f5
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-09-19 13:27:03 +0300

    Some changes

--- src/js/adding-to-favorites.js
+++ src/js/adding-to-favorites.js
@@ -1,64 +1,73 @@
-// import {
-//   calculationOfRatedStars,
-//   LS_DISHES_KEY,
-//   defaults,
-// } from './all-recipes.js';
-// import sprite from '../sprite.svg';
+import {
+  calculationOfRatedStars,
+  //   LS_DISHES_KEY,
+  //   defaults,
+} from './all-recipes.js';
+import sprite from '../sprite.svg';
 
-// const favouritesCardsList = document.querySelector('.js-all-favourite-cards');
-// console.log(favouritesCardsList);
+const defaults = {
+  preview: '../img/no-image-icon-23485.png',
+  title: 'no title',
+  description: 'no description',
+  rating: 'xx',
+};
 
-// const productsForFavoriteMarkup =
-//   JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
+const LS_DISHES_KEY = 'Favourite dishes';
 
-// function createFavoriteMarkup(arr) {
-//   console.log(arr);
-//   return arr
-//     .map(({ preview, title, description, rating, _id }) => {
-//       const ratedStars = calculationOfRatedStars(rating);
-//       const ratedStarsArray = Array.from(
-//         { length: ratedStars },
-//         () =>
-//           `<svg class="svg-star rated">
-//           <use href="${sprite}#icon-Star"></use>
-//         </svg>`
-//       ).join('');
+const favouritesCardsList = document.querySelector('.js-all-favorite-cards');
+console.log(favouritesCardsList);
 
-//       const notRatedStarsArray = Array.from(
-//         { length: 5 - ratedStars },
-//         () =>
-//           `<svg class="svg-star">
-//           <use href="${sprite}#icon-Star"></use>
-//         </svg>`
-//       ).join('');
+const productsForFavoriteMarkup =
+  JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
 
-//       return `<li class="card-item" data-id="${_id}">
-//           <svg class="card-svg-heart js-card-svg-heart" width="22px" height="22px">
-//         <use href="${sprite}#icon-heart"></use>
-//       </svg>
-//       <div class="image-gradient">
-//       <img class="card-img" src="${preview || defaults.preview}" alt="${
-//         title || defaults.title
-//       }"/>
-//       </div>
-//       <div class="card-text">
-//       <h2 class="card-dish-name">${title || defaults.title}</h2>
-//       <p class="card-dish-descr">${description || defaults.description}</p>
-//       </div>
-//       <div class="rating-btn-container">
+function createFavoriteMarkup(arr) {
+  console.log(arr);
+  return arr
+    .map(({ preview, title, description, rating, _id }) => {
+      const ratedStars = calculationOfRatedStars(rating);
+      const ratedStarsArray = Array.from(
+        { length: ratedStars },
+        () =>
+          `<svg class="svg-star rated">
+          <use href="${sprite}#icon-Star"></use>
+        </svg>`
+      ).join('');
 
-//           <p class="rating-number">${rating}</p>
-//           <div class="rating-container">
-//           ${ratedStarsArray}${notRatedStarsArray}
-//         </div>
-//         <button type="button" class="recipe-btn">See recipe</button>
-//       </div>
-//     </li>`;
-//     })
-//     .join('');
-// }
+      const notRatedStarsArray = Array.from(
+        { length: 5 - ratedStars },
+        () =>
+          `<svg class="svg-star">
+          <use href="${sprite}#icon-Star"></use>
+        </svg>`
+      ).join('');
 
-// favouritesCardsList.insertAdjacentHTML(
-//   'beforeend',
-//   createFavoriteMarkup(productsForFavoriteMarkup)
-// );
+      return `<li class="card-item" data-id="${_id}">
+          <svg class="card-svg-heart js-card-svg-heart" width="22px" height="22px">
+        <use href="${sprite}#icon-heart"></use>
+      </svg>
+      <div class="image-gradient">
+      <img class="card-img" src="${preview || defaults.preview}" alt="${
+        title || defaults.title
+      }"/>
+      </div>
+      <div class="card-text">
+      <h2 class="card-dish-name">${title || defaults.title}</h2>
+      <p class="card-dish-descr">${description || defaults.description}</p>
+      </div>
+      <div class="rating-btn-container">
+
+          <p class="rating-number">${rating}</p>
+          <div class="rating-container">
+          ${ratedStarsArray}${notRatedStarsArray}
+        </div>
+        <button type="button" class="recipe-btn">See recipe</button>
+      </div>
+    </li>`;
+    })
+    .join('');
+}
+
+favouritesCardsList.insertAdjacentHTML(
+  'beforeend',
+  createFavoriteMarkup(productsForFavoriteMarkup)
+);
--- src/js/all-recipes.js
+++ src/js/all-recipes.js
@@ -4,7 +4,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const categoriesBtn = document.querySelector('.js-all-categories-btn');
 const cardsList = document.querySelector('.js-card-list');
 export const LS_DISHES_KEY = 'Favourite dishes';
-let cardsInfo = [];
+export let cardsInfo = [];
 
 categoriesBtn.addEventListener('click', onAllCategoryButtonClick);
 
@@ -150,7 +150,8 @@ export function calculationOfRatedStars(rating) {
 
 // ================================= ADDING TO FAVOURITES ============================================
 
-let favouriteDishes = JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
+export let favouriteDishes =
+  JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
 
 cardsList.addEventListener('click', onAddingToFavourites);
 
@@ -175,3 +176,63 @@ export function onAddingToFavourites(event) {
   }
   localStorage.setItem(LS_DISHES_KEY, JSON.stringify(favouriteDishes));
 }
+
+// =========================== favorites page ========================
+
+// const favouritesCardsList = document.querySelector('.js-all-favorite-cards');
+// console.log(favouritesCardsList);
+
+// const productsForFavoriteMarkup =
+//   JSON.parse(localStorage.getItem(LS_DISHES_KEY)) ?? [];
+
+// function createFavoriteMarkup(arr) {
+//   console.log(arr);
+//   return arr
+//     .map(({ preview, title, description, rating, _id }) => {
+//       const ratedStars = calculationOfRatedStars(rating);
+//       const ratedStarsArray = Array.from(
+//         { length: ratedStars },
+//         () =>
+//           `<svg class="svg-star rated">
+//           <use href="${sprite}#icon-Star"></use>
+//         </svg>`
+//       ).join('');
+
+//       const notRatedStarsArray = Array.from(
+//         { length: 5 - ratedStars },
+//         () =>
+//           `<svg class="svg-star">
+//           <use href="${sprite}#icon-Star"></use>
+//         </svg>`
+//       ).join('');
+
+//       return `<li class="card-item" data-id="${_id}">
+//           <svg class="card-svg-heart js-card-svg-heart" width="22px" height="22px">
+//         <use href="${sprite}#icon-heart"></use>
+//       </svg>
+//       <div class="image-gradient">
+//       <img class="card-img" src="${preview || defaults.preview}" alt="${
+//         title || defaults.title
+//       }"/>
+//       </div>
+//       <div class="card-text">
+//       <h2 class="card-dish-name">${title || defaults.title}</h2>
+//       <p class="card-dish-descr">${description || defaults.description}</p>
+//       </div>
+//       <div class="rating-btn-container">
+
+//           <p class="rating-number">${rating}</p>
+//           <div class="rating-container">
+//           ${ratedStarsArray}${notRatedStarsArray}
+//         </div>
+//         <button type="button" class="recipe-btn">See recipe</button>
+//       </div>
+//     </li>`;
+//     })
+//     .join('');
+// }
+
+// favouritesCardsList.insertAdjacentHTML(
+//   'beforeend',
+//   createFavoriteMarkup(productsForFavoriteMarkup)
+// );
