aa4daa95f50dd1a078f5576a42df528c8500e55a
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-09-20 17:39:37 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/block-categories.js'
 import './js/list-of-popular.js'
 import './js/swiper'
-
+import './js/modal-recipe.js'
 import './js/adding-to-favorites.js'
 import './js/all-recipes.js'
